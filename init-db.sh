eeb4579bdd4cc27bfd5527331ca11aacf094d4e9
Andry Pebrianto <andrypeb227@gmail.com>
2026-06-13 19:52:32 +0700

    fix netcloud to postgre permisison

--- init-db.sh
+++ init-db.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 
 DB_NAME="${NEXTCLOUD_DB_NAME:-nextcloud}"
@@ -5,10 +6,16 @@ DB_NAME="${NEXTCLOUD_DB_NAME:-nextcloud}"
 echo "Checking if database '${DB_NAME}' exists..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOSQL
-  SELECT 'CREATE DATABASE "${DB_NAME}"'
+  SELECT 'CREATE DATABASE "${DB_NAME}" OWNER ${POSTGRES_USER}'
   WHERE NOT EXISTS (
     SELECT FROM pg_database WHERE datname = '${DB_NAME}'
   )\gexec
+
+  \c ${DB_NAME}
+
+  GRANT ALL PRIVILEGES ON DATABASE "${DB_NAME}" TO ${POSTGRES_USER};
+  GRANT ALL PRIVILEGES ON SCHEMA public TO ${POSTGRES_USER};
+  ALTER SCHEMA public OWNER TO ${POSTGRES_USER};
 EOSQL
 
 echo "Database '${DB_NAME}' is ready."
