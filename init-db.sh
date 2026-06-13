98a0a786eb61ebb09cc5c16ce6f8d0ea500d455c
Andry Pebrianto <andrypeb227@gmail.com>
2026-06-13 20:23:44 +0700

    change healt mongo again & fix postgre nextcloud

--- init-db.sh
+++ init-db.sh
@@ -10,12 +10,18 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EO
   WHERE NOT EXISTS (
     SELECT FROM pg_database WHERE datname = '${DB_NAME}'
   )\gexec
+EOSQL
 
-  \c ${DB_NAME}
-
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "${DB_NAME}" <<EOSQL
   GRANT ALL PRIVILEGES ON DATABASE "${DB_NAME}" TO ${POSTGRES_USER};
-  GRANT ALL PRIVILEGES ON SCHEMA public TO ${POSTGRES_USER};
   ALTER SCHEMA public OWNER TO ${POSTGRES_USER};
+  GRANT ALL ON SCHEMA public TO ${POSTGRES_USER};
+
+  -- Ini yang paling penting: cover tabel yang dibuat di masa depan
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT ALL ON TABLES TO ${POSTGRES_USER};
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT ALL ON SEQUENCES TO ${POSTGRES_USER};
 EOSQL
 
 echo "Database '${DB_NAME}' is ready."
